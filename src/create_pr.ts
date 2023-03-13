4e68fc911dd388a8be61d73f7c6b63c4eccc2051
Pavel Pochobradsky <pavlik.pocho@gmail.com>
2023-03-13 18:25:15 +0000

    Some changes

---
+++ src/create_pr.ts
@@ -0,0 +1,20 @@
+// Test GH token: process.env.GH_TOKEN
+
+async function createPR() {
+  
+}
+
+createPR()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
+
+// Run tests??
+// Create PR using octokit
+// Put in commit names from branch in Notion, which tasks have & haven't been completed, any custom user comment
+// Update branch & task status in Notion - done tasks should be changed to "PR to dev", rest left be
+// Keep in mind branch status is just a rollup of the status of its tasks since not all have to be completed for a PR
+// GH Action tests & checks??
+// Then it's a problem of release & GitHub actions for that...
